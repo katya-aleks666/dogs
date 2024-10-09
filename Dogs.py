259b69d54e1d7a47486035338b94b9fdceaf94b1
Dogs <ronincitadel@gmail.com>
2024-10-09 17:11:41 +0300

    Создана функция изображения в метку.

--- Dogs.py
+++ Dogs.py
@@ -1,9 +1,24 @@
 ftom tkinter import *
+from tkinter import message box as mb
 import requests
 from PIL import Image,Image TK
 from io import BytesIO
 
 
+
+def show image():
+    image_url=dog_get_image()
+    if image_ url:
+        try:
+            response=requests.get(image_url,stream=True)
+            response.raise_for_status()
+            img_data=Bytes.io(response.content)
+            img=Image.open(img_data)
+            img.thumbnail((300,300))
+            label.config(image=img)
+            label.image=img
+            except=Expection as e:
+            mb.showerror("ощибка")
 
 window=Tk()
 window.title("Картинки с собачками")
@@ -14,6 +29,7 @@ label.pack
 
 button=Button("Загрузить изображение",command=show_image)
 button.pack(pady=10)
+
 
 window.mainloop()
 
