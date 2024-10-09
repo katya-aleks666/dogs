d9859051e2d7f8fda528159b0a1564e710d298e3
Dogs <ronincitadel@gmail.com>
2024-10-09 18:03:11 +0300

    Создана функция изображения в метку.

--- Dogs.py
+++ Dogs.py
@@ -5,9 +5,18 @@ from PIL import Image,Image TK
 from io import BytesIO
 
 
+def get_dog_image
+    try:
+     response=requests.get
+     response.reis_for_status()
+     data=response.json()
+     return data('message')
+    except Exception as e:
+        mb.showerror(title:"Ощибка", message: f"Возникла ошибка при запросе к API {e}")
+
 
 def show image():
-    image_url=dog_get_image()
+    image_url=dog_get_image(https://dog.ceo/api/breeds/image/random)
     if image_ url:
         try:
             response=requests.get(image_url,stream=True)
@@ -15,10 +24,13 @@ def show image():
             img_data=Bytes.io(response.content)
             img=Image.open(img_data)
             img.thumbnail((300,300))
+            img=ImageTk.PhotoImage(img)
             label.config(image=img)
             label.image=img
             except=Expection as e:
-            mb.showerror("ощибка")
+            mb.showerror(title:"Ощибка",message:f"Возникла ошибка при загрузке изоюражения {e}")
+            return None
+
 
 window=Tk()
 window.title("Картинки с собачками")
